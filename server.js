23bc28335ed765d228cf8820aa01df9e17abe2ed
Roksana-Afrin-Anika <roksanaafrin47@gmail.com>
2024-12-06 17:23:52 +0600

    Added root route to backend

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 const path = require("path");
 const db = require("./db");
 const cors = require("cors");
@@ -8,8 +7,12 @@ app.use(cors());
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
+// Root route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Ecommerce API!");
+});
 // API routes
 const userRoutes = require("./Routes/userRoutes");
 app.use("/user", userRoutes);
@@ -27,14 +30,17 @@ const paymentRoutes = require("./Routes/paymentRoutes");
 app.use("/payment", paymentRoutes);
 
 // Serve static files from the React build directory
-app.use(express.static(path.join(__dirname, "Ecommerce_Website/my-app/build")));
+app.use(
+  express.static(path.join(__dirname, "Ecommerce_Website", "my-app", "build"))
+);
 
 // Serve the React app for any other requests (non-API)
 app.get("*", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "Ecommerce_Website/my-app/build", "index.html")
+    path.join(__dirname, "Ecommerce_Website", "my-app", "build", "index.html")
   );
 });
+
 app.listen(PORT, () => {
   console.log(`Ecommerce app listening on port ${PORT}`);
 });
